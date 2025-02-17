ffe3af18bed06677ca843b5cd9c969808475c2d4
Igor-Cegelnyk <il.cegelnik@gmail.com>
2025-02-17 23:30:07 +0200

    Add docker-compose.yml with pg & add db config to settings

--- backend/config/config.py
+++ backend/config/config.py
@@ -9,6 +9,22 @@ class RunConfig(BaseModel):
     port: int
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"),
@@ -18,6 +34,7 @@ class Settings(BaseSettings):
     )
 
     run: RunConfig
+    db: DatabaseConfig
 
 
 settings = Settings()
