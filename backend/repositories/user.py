48f29e9de5841598af5486cbefb4331f1381e527
Igor-Cegelnyk <il.cegelnik@gmail.com>
2025-02-19 00:10:35 +0200

    Added get_user_by_username to user repo

--- backend/repositories/user.py
+++ backend/repositories/user.py
@@ -1,6 +1,15 @@
+from typing import Annotated
+
+from sqlalchemy import select
+
 from backend.models import User
 from backend.repositories import SqlAlchemyRepository
 
 
 class UserRepository(SqlAlchemyRepository):
     model = User
+
+    async def get_user_by_username(self, username: str) -> Annotated[User, None]:
+        stmt = select(self.model).filter_by(username=username)
+        result = await self.session.scalar(stmt)
+        return result
