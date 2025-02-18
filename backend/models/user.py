df38556c7a0c64bbc164a642fd0f3ed804ff355f
Igor-Cegelnyk <il.cegelnik@gmail.com>
2025-02-18 22:42:23 +0200

    Refactored user model

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,23 +1,12 @@
-from typing import TYPE_CHECKING
-
-from fastapi_users_db_sqlalchemy import (
-    SQLAlchemyBaseUserTable,
-    SQLAlchemyUserDatabase,
-)
-from sqlalchemy import String
+from sqlalchemy import String, Boolean, LargeBinary
 from sqlalchemy.orm import Mapped, mapped_column
 
 from backend.models.base import Base
 from backend.models.mixins import IdIntPkMixin
 
-if TYPE_CHECKING:
-    from sqlalchemy.ext.asyncio import AsyncSession
 
-
-class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
-    first_name: Mapped[str] = mapped_column(String(100), nullable=False)
-    username: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
-
-    @classmethod
-    def get_db(cls, session: "AsyncSession"):
-        return SQLAlchemyUserDatabase(session, cls)
+class User(Base, IdIntPkMixin):
+    first_name: Mapped[str] = mapped_column(String(25), nullable=False)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(150), nullable=False)
+    active: Mapped[bool] = mapped_column(Boolean, default=True)
