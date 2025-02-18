53324ed5bd7e140f4e7e1c39904f1474394411f1
Igor-Cegelnyk <il.cegelnik@gmail.com>
2025-02-18 22:46:26 +0200

    Added lifespan to main

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,24 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
 from backend.config import settings
+from backend.database import db_helper
+from backend.routers import router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # Clean up the ML models and release the resources
+    db_helper.dispose()
 
 
 main_app = FastAPI()
 
+main_app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run(
