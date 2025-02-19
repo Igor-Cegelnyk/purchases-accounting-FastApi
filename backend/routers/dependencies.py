c51d4f354dca4d05130b312797586f231b4f0e6b
Igor-Cegelnyk <il.cegelnik@gmail.com>
2025-02-20 00:38:54 +0200

    Added validate_auth_user to dependencies

--- backend/routers/dependencies.py
+++ backend/routers/dependencies.py
@@ -1,15 +1,31 @@
 from typing import TYPE_CHECKING
 
-from fastapi import Depends
+from fastapi import Depends, Header
 
+from backend.authentication.strategies import decode_jwt
 from backend.database import db_helper
+from backend.models import User
 from backend.repositories import UserRepository
+from backend.schemas.user import UserRead
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-def get_user_repository(
+async def get_user_repository(
     session: "AsyncSession" = Depends(db_helper.session_getter),
 ) -> UserRepository:
     return UserRepository(session=session)
+
+
+async def validate_auth_user(
+    user_repo: UserRepository = Depends(get_user_repository),
+    token: str = Header(alias="x-auth-token"),
+) -> UserRead:
+    check_user = decode_jwt(token=token)
+    if not check_user:
+        pass
+
+    user = await user_repo.get_by_id(int(check_user["sub"]))
+
+    return user
