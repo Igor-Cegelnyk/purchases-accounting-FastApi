36a691fc938143fb5913b533654807b6cfd96570
Igor-Cegelnyk <il.cegelnik@gmail.com>
2025-02-20 23:59:21 +0200

    Refactored get_user_by_username in UserRepository

--- backend/repositories/user.py
+++ backend/repositories/user.py
@@ -9,7 +9,10 @@ from backend.repositories import SqlAlchemyRepository
 class UserRepository(SqlAlchemyRepository):
     model = User
 
-    async def get_user_by_username(self, username: str) -> Annotated[User, None]:
+    async def get_user_by_username(
+        self,
+        username: str,
+    ) -> Annotated[User, None]:
         stmt = select(self.model).filter_by(username=username)
         result = await self.session.scalar(stmt)
         return result
