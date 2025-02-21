79d5aa811511c3839467c061908fad56c69095a2
Igor-Cegelnyk <il.cegelnik@gmail.com>
2025-02-21 18:12:48 +0200

    Added .env.template to settings

--- backend/config/config.py
+++ backend/config/config.py
@@ -34,7 +34,9 @@ class ApiPrefix(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"),
+        env_file=os.path.join(
+            os.path.dirname(os.path.dirname(__file__)), ".env.template"
+        ),
         case_sensitive=False,
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
