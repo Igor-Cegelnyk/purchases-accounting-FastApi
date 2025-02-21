6cfc2eb472e73048e817733e0801d1670f9af060
Igor-Cegelnyk <il.cegelnik@gmail.com>
2025-02-21 19:34:59 +0200

    Fixed generate_keys.sh

--- backend/generate_keys.sh
+++ backend/generate_keys.sh
@@ -1,6 +1,6 @@
 set -e
 
-KEYS_DIR="/backend/authentication/keys"
+KEYS_DIR="/app/backend/authentication/keys"
 
 mkdir -p "$KEYS_DIR"
 
@@ -8,7 +8,7 @@ if [ ! -f "$KEYS_DIR/jwt-private.pem" ] || [ ! -f "$KEYS_DIR/jwt-public.pem" ]; 
     echo "Generating new JWT keys..."
     openssl genrsa -out "$KEYS_DIR/jwt-private.pem" 2048
     openssl rsa -in "$KEYS_DIR/jwt-private.pem" -outform PEM -pubout -out "$KEYS_DIR/jwt-public.pem"
-    chmod 600 "$KEYS_DIR/jwt-private.pem" "$KEYS_DIR/jwt-public.pem"
+    chmod 644 "$KEYS_DIR/jwt-private.pem" "$KEYS_DIR/jwt-public.pem"
 else
     echo "JWT keys already exist. Skipping key generation."
 fi
